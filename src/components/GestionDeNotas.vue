a11dacbebc818f412c672c02b1a35167164176a9
Carlos <carlos.castilla009@gmail.com>
2020-05-22 19:39:25 -0500

    Enlazando formulario con el modelo

---
+++ src/components/GestionDeNotas.vue
@@ -0,0 +1,37 @@
+<template>
+    <div id="app">
+        <h3>{{ msg }}</h3>
+        <div class="form">
+            <div class="form-group">
+                <label for="">Titulo</label>
+                <input class="form-control" type="text" v-model="nota.titulo">
+            </div>
+            <div class="form-group">
+                <label for="">Texto</label>
+                <textarea class="form-control" v-model="nota.texto"></textarea>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "GestionDeNotas",
+        data(){
+            return{
+                msg:'Gestion de Notas',
+                nota:{
+                    titulo:'',
+                    texto:''
+                }
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    .form{
+        text-align: left;
+    }
+
+</style>
