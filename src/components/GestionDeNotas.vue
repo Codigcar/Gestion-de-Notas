f9aae3daa5c450563c5bbf98593561da0f2223c4
CodigCar <carlos.castilla009@gmail.com>
2020-05-22 19:55:35 -0500

    Registrando Notas

--- src/components/GestionDeNotas.vue
+++ src/components/GestionDeNotas.vue
@@ -11,6 +11,7 @@
                 <textarea class="form-control" v-model="nota.texto"></textarea>
             </div>
         </div>
+        <button class="btn btn-primary" @click="agregarNota">Enviar</button>
     </div>
 </template>
 
@@ -23,7 +24,25 @@
                 nota:{
                     titulo:'',
                     texto:''
-                }
+                },
+                notas:[
+                    {
+                        titulo:'Ir al Colegio',
+                        texto:'Curso 1 a las 9.00 a.m.',
+                        fecha: new Date(Date.now()).toLocaleDateString()
+                    }
+                ]
+            }
+        },
+        methods:{
+            agregarNota:function () {
+                let {texto,titulo} = this.nota;
+                this.notas.push({
+                    texto,
+                    titulo,
+                    fecha: new Date(Date.now()).toLocaleString()
+                })
+
             }
         }
     }
